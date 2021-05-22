86a2bb3852a10427db65c1bf5008eba3a3a2aef4
gerald0mc <nickghur2005@gmail.com>
2021-05-22 00:04:03 -0500

    stuff

--- src/main/java/me/gavin/gavhack/module/impl/ClickGUI.java
+++ src/main/java/me/gavin/gavhack/module/impl/ClickGUI.java
@@ -1,20 +1,24 @@
 package me.gavin.gavhack.module.impl;
 
+import me.gavin.gavhack.Gavhack;
 import me.gavin.gavhack.module.Category;
 import me.gavin.gavhack.module.Module;
 import me.gavin.gavhack.setting.BooleanSetting;
+import me.gavin.gavhack.setting.NumberSetting;
 import org.lwjgl.input.Keyboard;
 
 public class ClickGUI extends Module {
 
     public final BooleanSetting descriptions = new BooleanSetting(this, "Descriptions", true);
     public final BooleanSetting background = new BooleanSetting(this, "Background", false);
+    public final BooleanSetting transMode = new BooleanSetting(this, "Trans Mode", true);
 
     public ClickGUI() {
         super("ClickGUI", "Manage modules with a GUI", Category.Client);
         this.keybind.bind = Keyboard.KEY_RSHIFT;
         this.settings.add(descriptions);
         this.settings.add(background);
+        settings.add(transMode);
     }
 
     @Override
